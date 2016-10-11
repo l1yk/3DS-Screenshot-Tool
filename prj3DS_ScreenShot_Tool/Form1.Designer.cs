2b9d40a074bf0f26b1b19d0a582497188bb0423c
l1yk <l1yk.tw@gmail.com>
2016-10-11 09:21:39 +0800

    First Upload

---
+++ prj3DS_ScreenShot_Tool/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace prj3DS_ScreenShot_Tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.檔案FToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.關閉程式ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.picTopScreen = new System.Windows.Forms.PictureBox();
+            this.picBottomScreen = new System.Windows.Forms.PictureBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsBtnOpenTop = new System.Windows.Forms.ToolStripButton();
+            this.tsBtnOpenBottom = new System.Windows.Forms.ToolStripButton();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picTopScreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBottomScreen)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.檔案FToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.menuStrip1.Size = new System.Drawing.Size(400, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 檔案FToolStripMenuItem
+            // 
+            this.檔案FToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.關閉程式ToolStripMenuItem});
+            this.檔案FToolStripMenuItem.Name = "檔案FToolStripMenuItem";
+            this.檔案FToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.檔案FToolStripMenuItem.Text = "檔案(&F)";
+            // 
+            // 關閉程式ToolStripMenuItem
+            // 
+            this.關閉程式ToolStripMenuItem.Name = "關閉程式ToolStripMenuItem";
+            this.關閉程式ToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.關閉程式ToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.關閉程式ToolStripMenuItem.Text = "關閉程式";
+            this.關閉程式ToolStripMenuItem.Click += new System.EventHandler(this.關閉程式ToolStripMenuItem_Click);
+            // 
+            // picTopScreen
+            // 
+            this.picTopScreen.BackColor = System.Drawing.Color.Black;
+            this.picTopScreen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picTopScreen.Location = new System.Drawing.Point(0, 0);
+            this.picTopScreen.Margin = new System.Windows.Forms.Padding(0);
+            this.picTopScreen.Name = "picTopScreen";
+            this.picTopScreen.Size = new System.Drawing.Size(400, 240);
+            this.picTopScreen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picTopScreen.TabIndex = 1;
+            this.picTopScreen.TabStop = false;
+            // 
+            // picBottomScreen
+            // 
+            this.picBottomScreen.BackColor = System.Drawing.Color.Black;
+            this.picBottomScreen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picBottomScreen.Location = new System.Drawing.Point(40, 0);
+            this.picBottomScreen.Margin = new System.Windows.Forms.Padding(0);
+            this.picBottomScreen.Name = "picBottomScreen";
+            this.picBottomScreen.Size = new System.Drawing.Size(320, 240);
+            this.picBottomScreen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBottomScreen.TabIndex = 2;
+            this.picBottomScreen.TabStop = false;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripSeparator1,
+            this.tsBtnOpenTop,
+            this.tsBtnOpenBottom});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.toolStrip1.Size = new System.Drawing.Size(400, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "儲存完整螢幕截圖";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // tsBtnOpenTop
+            // 
+            this.tsBtnOpenTop.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsBtnOpenTop.Image = ((System.Drawing.Image)(resources.GetObject("tsBtnOpenTop.Image")));
+            this.tsBtnOpenTop.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsBtnOpenTop.Name = "tsBtnOpenTop";
+            this.tsBtnOpenTop.Size = new System.Drawing.Size(95, 22);
+            this.tsBtnOpenTop.Text = "開啟上螢幕截圖";
+            this.tsBtnOpenTop.Click += new System.EventHandler(this.tsBtnOpenTop_Click);
+            // 
+            // tsBtnOpenBottom
+            // 
+            this.tsBtnOpenBottom.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsBtnOpenBottom.Image = ((System.Drawing.Image)(resources.GetObject("tsBtnOpenBottom.Image")));
+            this.tsBtnOpenBottom.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsBtnOpenBottom.Name = "tsBtnOpenBottom";
+            this.tsBtnOpenBottom.Size = new System.Drawing.Size(95, 22);
+            this.tsBtnOpenBottom.Text = "開啟下螢幕截圖";
+            this.tsBtnOpenBottom.Click += new System.EventHandler(this.tsBtnOpenBottom_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.picTopScreen, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 49);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(400, 480);
+            this.tableLayoutPanel1.TabIndex = 7;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel2.Controls.Add(this.picBottomScreen, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 240);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(400, 240);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 529);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("微軟正黑體", 8F);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(416, 568);
+            this.Name = "Form1";
+            this.Text = "3DS截圖處理工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picTopScreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBottomScreen)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.PictureBox picTopScreen;
+        private System.Windows.Forms.PictureBox picBottomScreen;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton tsBtnOpenTop;
+        private System.Windows.Forms.ToolStripButton tsBtnOpenBottom;
+        private System.Windows.Forms.ToolStripMenuItem 檔案FToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 關閉程式ToolStripMenuItem;
+    }
+}
+
---
+++ prj3DS_ScreenShot_Tool/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prj3DS_ScreenShot_Tool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<DriveInfo> LstRemovableDrives = new List<DriveInfo>();
+            List<string> LstValidScreenshotFiles = new List<string>();
+
+            foreach (DriveInfo tmpDrive in DriveInfo.GetDrives())
+            {
+                if (!tmpDrive.Name.Equals(@"A:\"))
+                {
+                    if (tmpDrive.DriveType.Equals(DriveType.Removable))
+                    {
+                        LstRemovableDrives.Add(tmpDrive);
+                    }
+                }
+            }
+
+            foreach (DriveInfo tmpDrive in LstRemovableDrives)
+            {
+                DirectoryInfo tmpDirInfo = new DirectoryInfo(tmpDrive.Name);
+                List<FileInfo> tmpBmpFileList = new List<FileInfo>();
+                tmpBmpFileList.AddRange(tmpDirInfo.GetFiles("*.bmp", SearchOption.TopDirectoryOnly));
+                foreach (FileInfo tmpFileInfo in tmpBmpFileList)
+                {
+                    if (tmpFileInfo.Name.IndexOf("top_").Equals(0))
+                    {
+                        if (File.Exists(tmpDirInfo.FullName + tmpFileInfo.Name.Replace("top_", "bot_")))
+                        {
+                            LstValidScreenshotFiles.Add(tmpDirInfo.Name + tmpFileInfo.Name);
+                        }
+                    }
+                }
+            }
+
+            if (LstValidScreenshotFiles.Count > 0)
+            {
+                picTopScreen.Image = Image.FromFile(LstValidScreenshotFiles[LstValidScreenshotFiles.Count - 1]);
+                picBottomScreen.Image = Image.FromFile(LstValidScreenshotFiles[LstValidScreenshotFiles.Count - 1].Replace("\\top_", "\\bot_"));
+            }
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            Bitmap picRslt;
+            if (picTopScreen.Image != null && picBottomScreen.Image != null)
+            {
+                picRslt = (Bitmap)MergeScreenshot(picTopScreen.Image, picBottomScreen.Image);
+            }
+            else
+            {
+                if (picTopScreen.Image != null & picBottomScreen.Image == null)
+                {
+                    Bitmap bitTopScreen = (Bitmap)picTopScreen.Image;
+                    Bitmap bitBlack = new Bitmap(320, 240);
+                    bitBlack.SetResolution(bitTopScreen.HorizontalResolution, bitTopScreen.VerticalResolution);
+                    using (Graphics gr = Graphics.FromImage(bitBlack))
+                    {
+                        gr.Clear(Color.Black);
+                    }
+                    picRslt = (Bitmap)MergeScreenshot(bitTopScreen, bitBlack);
+                }
+                else if (picTopScreen.Image == null & picBottomScreen.Image != null)
+                {
+                    Bitmap bitBottomScreen = (Bitmap)picTopScreen.Image;
+                    Bitmap bitBlack = new Bitmap(400, 240);
+                    bitBlack.SetResolution(bitBottomScreen.HorizontalResolution, bitBottomScreen.VerticalResolution);
+                    using (Graphics gr = Graphics.FromImage(bitBlack))
+                    {
+                        gr.Clear(Color.Black);
+                    }
+                    picRslt = (Bitmap)MergeScreenshot(bitBlack, bitBottomScreen);
+                }
+                else
+                {
+                    Bitmap bitTopBlack = new Bitmap(400, 240);
+                    bitTopBlack.SetResolution(72, 72);
+                    using (Graphics gr = Graphics.FromImage(bitTopBlack))
+                    {
+                        gr.Clear(Color.Black);
+                    }
+                    Bitmap bitBottomBlack = new Bitmap(320, 240);
+                    bitBottomBlack.SetResolution(72, 72);
+                    using (Graphics gr = Graphics.FromImage(bitBottomBlack))
+                    {
+                        gr.Clear(Color.Black);
+                    }
+                    picRslt = (Bitmap)MergeScreenshot(bitTopBlack, bitBottomBlack);
+                }
+            }
+
+            //Form tmpFrm = new Form();
+            //PictureBox tmpPb = new PictureBox();
+            //tmpPb.Image = picRslt;
+            //tmpPb.SizeMode = PictureBoxSizeMode.CenterImage;
+            //tmpPb.Width = 400;
+            //tmpPb.Height = 480;
+            //tmpFrm.Controls.Add(tmpPb);
+            //tmpFrm.ClientSize = new System.Drawing.Size(400, 480);
+            //tmpFrm.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            //tmpFrm.MaximizeBox = false;
+            //tmpFrm.Show();
+
+            SaveFileDialog fSaveDiag = new SaveFileDialog();
+            fSaveDiag.FileName = "3DS_ScreenShot.png";
+            fSaveDiag.Filter = "Images (*.png, *.bmp, *.jpg)|*.png;*.bmp;*.jpg";
+            ImageFormat format = ImageFormat.Png;
+            if (fSaveDiag.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                try
+                {
+                    string ext = System.IO.Path.GetExtension(fSaveDiag.FileName);
+                    switch (ext)
+                    {
+                        case ".jpg":
+                            format = ImageFormat.Jpeg;
+                            break;
+                        case ".bmp":
+                            format = ImageFormat.Bmp;
+                            break;
+                    }
+                    picRslt.Save(fSaveDiag.FileName, format);
+
+                    MessageBox.Show("儲存成功！");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void tsBtnOpenTop_Click(object sender, EventArgs e)
+        {
+            LoadTopScreen();
+        }
+
+        private void tsBtnOpenBottom_Click(object sender, EventArgs e)
+        {
+            LoadBottomScreen();
+        }
+
+        private void 關閉程式ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ExitApplication();
+        }
+
+        private Image MergeScreenshot(Image top, Image bot)
+        {
+            Bitmap imgRslt = new Bitmap(400, 480);
+            imgRslt.SetResolution(top.HorizontalResolution, top.VerticalResolution);
+
+            Graphics gr = Graphics.FromImage(imgRslt);
+
+            gr.Clear(Color.Black);
+            gr.DrawImageUnscaled(top, 0, 0);
+            gr.DrawImageUnscaled(bot, 40, 240);
+
+            gr.Dispose();
+
+            return imgRslt;
+        }
+
+        private void LoadTopScreen()
+        {
+            OpenFileDialog fOpenDiag = new OpenFileDialog();
+            fOpenDiag.Filter = "Image File (*.bmp, *.png, *.jpg)|*.bmp;*.png;*.jpg";
+            fOpenDiag.FilterIndex = 1;
+            fOpenDiag.RestoreDirectory = true;
+
+            Stream tmpStream = null;
+
+            if (fOpenDiag.ShowDialog().Equals(DialogResult.OK))
+            {
+                try
+                {
+                    if ((tmpStream = fOpenDiag.OpenFile()) != null)
+                    {
+                        using (tmpStream)
+                        {
+                            Bitmap topImage = (Bitmap)Image.FromStream(tmpStream);
+                            if (topImage.Width.Equals(400) && topImage.Height.Equals(240))
+                                picTopScreen.Image = topImage;
+                            else
+                                throw new Exception("圖片大小不正確");
+                        }
+
+                        string[] tmpFileNameStruct = fOpenDiag.FileName.Split('\\');
+                        if (tmpFileNameStruct[tmpFileNameStruct.Length - 1].IndexOf("top_").Equals(0))
+                        {
+                            tmpFileNameStruct[tmpFileNameStruct.Length - 1] = tmpFileNameStruct[tmpFileNameStruct.Length - 1].Replace("top_", "bot_");
+                            string tmpPath = String.Join("\\", tmpFileNameStruct);
+                            if (File.Exists(tmpPath))
+                            {
+                                if (MessageBox.Show("是否載入相對應的下螢幕截圖？", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Question).Equals(DialogResult.Yes))
+                                {
+                                    Bitmap botImage = (Bitmap)Image.FromFile(tmpPath);
+                                    if (botImage.Width.Equals(320) && botImage.Height.Equals(240))
+                                        picBottomScreen.Image = botImage;
+                                    else
+                                        throw new Exception("圖片大小不正確");
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void LoadBottomScreen()
+        {
+            OpenFileDialog fOpenDiag = new OpenFileDialog();
+            fOpenDiag.Filter = "Image File (*.bmp, *.png, *.jpg)|*.bmp;*.png;*.jpg";
+            fOpenDiag.FilterIndex = 1;
+            fOpenDiag.RestoreDirectory = true;
+
+            Stream tmpStream = null;
+
+            if (fOpenDiag.ShowDialog().Equals(DialogResult.OK))
+            {
+                try
+                {
+                    if ((tmpStream = fOpenDiag.OpenFile()) != null)
+                    {
+                        using (tmpStream)
+                        {
+                            Bitmap topImage = (Bitmap)Image.FromStream(tmpStream);
+                            if (topImage.Width.Equals(320) && topImage.Height.Equals(240))
+                                picBottomScreen.Image = topImage;
+                            else
+                                throw new Exception("圖片大小不正確");
+                        }
+                    }
+
+                    string[] tmpFileNameStruct = fOpenDiag.FileName.Split('\\');
+                    if (tmpFileNameStruct[tmpFileNameStruct.Length - 1].IndexOf("bot_").Equals(0))
+                    {
+                        tmpFileNameStruct[tmpFileNameStruct.Length - 1] = tmpFileNameStruct[tmpFileNameStruct.Length - 1].Replace("bot_", "top_");
+                        string tmpPath = String.Join("\\", tmpFileNameStruct);
+                        if (File.Exists(tmpPath))
+                        {
+                            if (MessageBox.Show("是否載入相對應的上螢幕截圖？", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Question).Equals(DialogResult.Yes))
+                            {
+                                Bitmap topImage = (Bitmap)Image.FromFile(tmpPath);
+                                if (topImage.Width.Equals(400) && topImage.Height.Equals(240))
+                                    picTopScreen.Image = topImage;
+                                else
+                                    throw new Exception("圖片大小不正確");
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void ExitApplication()
+        {
+            Close();
+        }
+    }
+}
